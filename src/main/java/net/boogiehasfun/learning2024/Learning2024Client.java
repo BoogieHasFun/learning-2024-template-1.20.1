62e2c5f0d42ea359127bc4e0c01a1cb51091429b
boogie <76754631+BoogieHasFun@users.noreply.github.com>
2024-01-05 20:37:30 -0500

    Initial commit

---
+++ src/main/java/net/boogiehasfun/learning2024/Learning2024Client.java
@@ -0,0 +1,10 @@
+package net.boogiehasfun.learning2024;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Learning2024Client implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/boogiehasfun/learning2024/Learning2024DataGenerator.java
@@ -0,0 +1,11 @@
+package net.boogiehasfun.learning2024;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Learning2024DataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
